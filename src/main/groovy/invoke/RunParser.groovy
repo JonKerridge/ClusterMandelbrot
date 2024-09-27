dd08b957401f9e17b6c5a2e32742f22ddcadd55f
JonKe <jonkerridge@btinternet.com>
2024-09-27 18:46:35 +0100

    Now uses cluster_cli version 1.0.10

--- src/main/groovy/invoke/RunParser.groovy
+++ src/main/groovy/invoke/RunParser.groovy
@@ -5,7 +5,7 @@ import cluster_cli.parse.Parser
 class RunParser {
   static void main(String[] args) {
     String workingDirectory = System.getProperty("user.dir")
-    Parser parser = new Parser("$workingDirectory/src/main/groovy/mandelbrotDSLfiles/mandelbrot1n4w2048")
+    Parser parser = new Parser("$workingDirectory/src/main/groovy/mandelbrotDSLfiles/mandelbrot1n4w512")
     assert parser.parse() :"Parsing failed"
   }
 
